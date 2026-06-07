0d2e798f9ac031fc0ced77c59cdb2a804a2ae666
jax <273277355@qq.com>
2026-06-07 23:14:35 +0800

    init

---
+++ CrazyDashboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrazyDashboard
+//
+//  Created by 邓杰豪 on 7/6/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
