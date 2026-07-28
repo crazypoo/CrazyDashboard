680b480916dff64968dc9a9b0485f59b0fdc122f
jax <1111976+crazypoo@users.noreply.github.com>
2026-07-28 16:51:06 +0800

    ex

---
+++ PTSpeed/PTCarPlaySceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  PTCarPlaySceneDelegate.swift
+//  PTSpeed
+//
+//  Created by 邓杰豪 on 28/7/2026.
+//
+
+import UIKit
+import CarPlay
+import PooTools
+import SnapKit
+import SwifterSwift
+
+class PTCarPlaySceneDelegate: UIResponder,CPTemplateApplicationSceneDelegate {
+    var interfaceController: CPInterfaceController?
+    var carWindow: CPWindow?
+
+    let dashboardVC = ViewController()
+    
+    // 当插上数据线，CarPlay 启动时调用
+    func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+                                  didConnect interfaceController: CPInterfaceController,
+                                  to window: CPWindow) {
+        
+        self.interfaceController = interfaceController
+        self.carWindow = window
+        
+        // 1. 创建地图模板作为底层（这是拿到 Window 权限的前提）
+        let mapTemplate = CPMapTemplate()
+        interfaceController.setRootTemplate(mapTemplate, animated: true, completion: nil)
+        window.rootViewController = dashboardVC
+    }
+}
